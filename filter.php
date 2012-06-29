087f91442b242f298768fd2a36f584e18687988c
Bruno Freitas <freitas@setic.ufsc.br>
2012-06-29 16:31:49 -0300

    YUI script para fazer o show/hide na barra de filtragem

--- filter.php
+++ filter.php
@@ -11,24 +11,7 @@ class filter_tutor_polo extends moodleform {
         global $CFG;
 
         $mform = & $this->_form;
-        //javascrip para ocultar as divs tutor/polo
-        $mform->addElement('html',
-                '<script type="text/javascript">
-                    function toggleDivs(val){
-                        if(val == "tutor" || val === true)
-                        {
-                            document.getElementById("div_tutor").style.display = "none";
-                            document.getElementById("div_polo").style.display = "block";
-                        }
-                        else if(val == "polo" || val === false){
-                            document.getElementById("div_polo").style.display = "none";
-                            document.getElementById("div_tutor").style.display = "block";
-                        }
-                    }
-                 </script>');
                 
-        
-        //verificar a necessidade de criar um fieldset antes do legend
         $mform->addElement('header','filter_legend', 'Filtrar Estudantes');
         
         //select box do estado da avaliacao
@@ -37,23 +20,20 @@ class filter_tutor_polo extends moodleform {
          
         
         //radio button da filtragem do tipo de estudante, por tutor ou polo
-        $attributes = "";
         $radioarray = array();
-        $radioarray[] = &MoodleQuickForm::createElement('radio', 'tutor_polo', 'tutor', 'por Tutor', "tutor", 
-                            array('onclick'=> 'toggleDivs(!this.checked)'));
-        $radioarray[] = &MoodleQuickForm::createElement('radio', 'tutor_polo', 'polo', 'por Polo', "polo", 
-                            array('onclick'=> 'toggleDivs(this.checked)'));
-        $mform->addGroup($radioarray, 'radio_tutor_polo', 'Filtrar Estudantes: ');
+        $radioarray[] = &MoodleQuickForm::createElement('radio', 'radiofilter', 'tutor', 'por Tutor', 'tutor');
+        $radioarray[] = &MoodleQuickForm::createElement('radio', 'radiofilter', 'polo', 'por Polo', 'polo');
+        $mform->addGroup($radioarray, '', 'Filtrar Estudantes: ', null);
     
         
         //div dos nomes dos tutores
-        $mform->addElement('html','<div class="div_tutor" id="div_tutor" >');
+        $mform->addElement('html','<div class="relatorio-unasus filter div_list_show" id="div_tutor" >');
         $multiple_tutor = & $mform->addElement('select', 'multiple_tutor',"Tutores",get_nomes_tutores()); 
         $multiple_tutor->setMultiple(true);
         $mform->addElement('html','</div>');
         
         //div dos nomes dos polos
-        $mform->addElement('html','<div class="div_polo" id="div_polo">');
+        $mform->addElement('html','<div class="relatorio-unasus filter div_list_show" id="div_polo">');
         $multiple_tutor = & $mform->addElement('select', 'multiple_polo',"Polos", get_nomes_polos()); 
         $multiple_tutor->setMultiple(true);
         $mform->addElement('html','</div>');
--- module.js
+++ module.js
@@ -1,10 +1,21 @@
 M.report_unasus = {};
 
 M.report_unasus.init = function(Y) {
+    node_tutor = Y.one('#div_tutor');
+    node_tutor.hide();
+    
+    node_polo = Y.one('#div_polo');
+    node_polo.hide();
     
     Y.on('click', function(e) {
-        alert('Hellow wolololow');
-    },'#id_situacao' );
+        node_tutor.show();
+        node_polo.hide();
+    },'#id_radiofilter_tutor' );
+    
+    Y.on('click', function(e) {
+        node_tutor.hide();
+        node_polo.show();
+    },'#id_radiofilter_polo' );
     
 };
 
