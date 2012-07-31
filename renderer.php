d32e5d5082eadc6a783dc0d48e70e18871bc25c8
Bruno Freitas <freitas@setic.ufsc.br>
2012-07-31 19:09:30 -0300

    termino da legenda e inclusão das imagens

--- renderer.php
+++ renderer.php
@@ -118,66 +118,41 @@ class report_unasus_renderer extends plugin_renderer_base {
               array('action'=>"{$CFG->wwwroot}/report/unasus/index.php?relatorio={$this->report}",
                     'method'=>'post', 'accept-charset'=>'utf-8', 'id'=>'filter_form'));
 
-        $output .= html_writer::start_tag('fieldset', array('class'=>''));
+        $output .= html_writer::start_tag('fieldset', array('class'=>'relatorio-unasus fieldset'));
         $output .= html_writer::nonempty_tag('legend', 'Filtrar Estudantes');
 
         $output .= html_writer::start_tag('div');
         $output .= html_writer::empty_tag('input', array('type'=>'hidden', 'id'=>'report_hidden', 'value'=>"$this->report"));
 
-        $output .= html_writer::select(array('Em Aberto', 'Em Dia', 'Expirado', 'Fora do Prazo'), 'prazo_select', '', false);
+        $output .= html_writer::label('Estado da Atividade: ', 'select_estado');
+        $output .= html_writer::select(array('Em Aberto', 'Em Dia', 'Expirado', 'Fora do Prazo'), 'prazo_select', '', false,array('id'=>'select_estado'));
 
         $output .= html_writer::start_tag('div');
 
-        $output .= html_writer::start_tag('div', array('class'=>'relatorio-unasus multiple_list'));
-        $output .= html_writer::nonempty_tag('p', 'Modulos');
-        $output .= html_writer::select(get_nomes_modulos(), 'multiple_modulo', '', false, array('multiple'=>'multiple'));
+        $output .= html_writer::start_tag('div', array('class'=>'multiple_list'));
+        $output .= html_writer::label('Filtrar Modulos:', 'multiple_modulo');
+        $output .= html_writer::select(get_nomes_modulos(), 'multiple_modulo', '', false, array('multiple'=>'multiple','id'=>'multiple_modulo'));
         $output .= html_writer::end_tag('div');
 
-        $output .= html_writer::start_tag('div', array('class'=>'relatorio-unasus multiple_list'));
-        $output .= html_writer::nonempty_tag('p', 'Polos');
-        $output .= html_writer::select(get_nomes_polos(), 'multiple_polo', '', false, array('multiple'=>'multiple'));
+        $output .= html_writer::start_tag('div', array('class'=>'multiple_list'));
+        $output .= html_writer::label('Filtrar Polos:', 'multiple_polo');
+        $output .= html_writer::select(get_nomes_polos(), 'multiple_polo', '', false, array('multiple'=>'multiple','id'=>'multiple_polo'));
         $output .= html_writer::end_tag('div');
 
-        $output .= html_writer::start_tag('div', array('class'=>'relatorio-unasus multiple_list'));
-        $output .= html_writer::nonempty_tag('p', 'Tutores');
-        $output .= html_writer::select(get_nomes_tutores(), 'multiple_tutor', '', false, array('multiple'=>'multiple'));
+        $output .= html_writer::start_tag('div', array('class'=>'multiple_list'));
+        $output .= html_writer::label('Filtrar Tutores:', 'multiple_tutor');
+        $output .= html_writer::select(get_nomes_tutores(), 'multiple_tutor', '', false, array('multiple'=>'multiple','id'=>'multiple_tutor'));
         $output .= html_writer::end_tag('div');
 
         $output .= html_writer::end_tag('div');
 
 
-
-
-
-//        $mform->addElement('header', 'filter_legend', 'Filtrar Estudantes');
-//        $mform->addElement('hidden', 'relatorio', $this-
-//        >_customdata['relatorio']);
-//
-//        //select box do estado da avaliacao
-//        $mform->addElement('select', 'situacao', "Situação:", array("Em Aberto", "Em Dia", "Expirado", "Fora do Prazo"));
-//
-//        //multi-select dos módulos
-//        $multiple_modulos = &$mform->createElement('select', 'multiple_modulos', "Módulos", get_nomes_modulos());
-//        $multiple_modulos->setMultiple(true);
-//
-//        //multi-select dos tutores
-//        $multiple_tutor = &$mform->createElement('select', 'multiple_tutor', "Tutores", get_nomes_tutores());
-//        $multiple_tutor->setMultiple(true);
-//
-//        //multi-select dos polos
-//        $multiple_polo = &$mform->createElement('select', 'multiple_polo', "Polos", get_nomes_polos());
-//        $multiple_polo->setMultiple(true);
-//
-//        //group para os multi-selects
-//        $lists_array = array();
-//        $lists_array[] = $multiple_modulos;
-//        $lists_array[] = $multiple_tutor;
-//        $lists_array[] = $multiple_polo;
-//        $mform->addGroup($lists_array, '', 'Filtrar por Módulo: ',
-//                                            array('Filtrar por Tutor: ','Filtrar por Polo: '));
-//
-//        //submit button, ainda falta alinhar ele
-//        $mform->addElement('submit', 'button_filtrar', 'Filtrar', array('class' => 'filter'));
+        $output .= html_writer::empty_tag('input', array('type'=>'radio','name'=>'modo_exibicao','value'=>'tabela','id'=>'radio_tabela'));
+        $output .= html_writer::label("<img src=\"{$CFG->wwwroot}/report/unasus/img/table.png\">Tabela de Dados", 'radio_tabela',true,array('class'=>'radio'));
+        $output .= html_writer::empty_tag('input', array('type'=>'radio','name'=>'modo_exibicao','value'=>'grafico_valor','id'=>'radio_valores'));
+        $output .= html_writer::label("<img src=\"{$CFG->wwwroot}/report/unasus/img/chart.png\">Gráfico de Valores", 'radio_valores',true,array('class'=>'radio'));
+        $output .= html_writer::empty_tag('input', array('type'=>'radio','name'=>'modo_exibicao','value'=>'grafico_porcentagens','id'=>'radio_porcentagem'));
+        $output .= html_writer::label("<img src=\"{$CFG->wwwroot}/report/unasus/img/pct.png\">Gráfico de Porcentagem", 'radio_porcentagem',true,array('class'=>'radio'));
 
         $output .= html_writer::empty_tag('input', array('type'=>'submit', 'value'=>'Filtrar'));
         $output .= html_writer::end_tag('div');
