f6f548167f4a48eff0ee399971c4cb073c5222a1
Gabriel Mazetto <brodock@gmail.com>
2012-11-07 21:39:03 -0200

    Restringe a listagem de relatórios de uso do sistema
    Somente quem ten a capability report/unasus:view_all (Coordenadores) pode acessar esses relatórios.

--- lib.php
+++ lib.php
@@ -9,6 +9,10 @@ function report_unasus_relatorios_validos_list() {
         'potenciais_evasoes');
 }
 
+function report_unasus_relatorios_restritos_list() {
+    return array('acesso_tutor', 'uso_sistema_tutor');
+}
+
 /**
  * @param navigation_node $navigation
  * @param stdClass $course
@@ -16,10 +20,16 @@ function report_unasus_relatorios_validos_list() {
  */
 function report_unasus_extend_navigation_course($navigation, $course, $context) {
 
+    $restricted_list = report_unasus_relatorios_restritos_list();
+
     if ($course->id != SITEID && has_capability('report/unasus:view', $context)) {
         $reports = report_unasus_relatorios_validos_list();
 
         foreach($reports as $report) {
+            if (in_array($report, $restricted_list) && !has_capability('report/unasus:view_all', $context)) {
+                continue;
+            }
+
             $url = new moodle_url('/report/unasus/index.php', array('relatorio' => $report, 'course' => $course->id));
             $navigation->add(get_string($report, 'report_unasus'), $url, navigation_node::TYPE_SETTING, null, $report, new pix_icon('i/report', ''));
         }
