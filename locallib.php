77558b3c8210520de046e0071c1e8c4342222fb5
Gabriel Mazetto <brodock@gmail.com>
2012-12-13 21:05:52 -0200

    Trocando caminho do Grupo de Tutoria para novo plugin (local)

--- locallib.php
+++ locallib.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-require_once("{$CFG->dirroot}/{$CFG->admin}/tool/tutores/middlewarelib.php");
-require_once("$CFG->dirroot/$CFG->admin/tool/tutores/lib.php");
+require_once("{$CFG->dirroot}/local/tutores/middlewarelib.php");
+require_once("{$CFG->dirroot}/local/tutores/lib.php");
 require_once($CFG->dirroot . '/report/unasus/datastructures.php');
 //require_once($CFG->dirroot . '/report/unasus/dados.php');
 require_once($CFG->dirroot . '/report/unasus/relatorios/relatorios.php');
