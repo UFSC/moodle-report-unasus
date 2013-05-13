295df9ee058f947abea13ec62e26fed2ecfabcdf
Gabriel Mazetto <brodock@gmail.com>
2013-05-13 17:42:59 -0300

    Versão incrementada para 20130513

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2013042900; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2013051300; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2012062501; // Requires this Moodle version (2.3.1+)
 $plugin->component = 'report_unasus'; // Full name of the plugin (used for diagnostics)
