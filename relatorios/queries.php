1107d3aaeefe2050135f3eddea8a0765eb6fc998
Gabriel Mazetto <brodock@gmail.com>
2014-07-01 18:11:29 -0300

    Versão 2013070100

--- relatorios/queries.php
+++ relatorios/queries.php
@@ -397,7 +397,7 @@ function query_acesso_tutor() {
                 ON (u.id=l.userid)
                    {$filtro_tutor}
           GROUP BY calendar_year, calendar_month, calendar_day, u.id
-          ORDER BY calendar_year, calendar_month, calendar_day, u.firstname";
+          ORDER BY calendar_year, calendar_month, calendar_day";
 }
 
 function query_lti() {
