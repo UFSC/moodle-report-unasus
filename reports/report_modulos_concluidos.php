f48766d60059c883ddc26eebc3d0d947eb128dfc
salazar <luizhsalazar@gmail.com>
2015-06-30 16:28:13 -0300

    Inserindo nova estrutura para o relatório de módulos concluídos
    Nova estrutura do relatório de módulos concluídos sem acesso ao arquivo
    do loop.

--- reports/report_modulos_concluidos.php
+++ reports/report_modulos_concluidos.php
@@ -33,83 +33,95 @@ class report_modulos_concluidos extends Factory {
 
     public function get_dados(){
 
+        global $DB;
+
         $modulos = $this->modulos_selecionados;
 
-        // Consulta
-        $query_atividades_nao_postadas = query_atividades();
-        $query_quiz = query_quiz();
-        $query_forum = query_postagens_forum();
-
         // Recupera dados auxiliares
-        $nomes_cohorts = get_nomes_cohorts($this->get_categoria_curso_ufsc());
         $nomes_estudantes = grupos_tutoria::get_estudantes($this->get_categoria_turma_ufsc());
-        $nomes_polos = get_polos($this->get_categoria_turma_ufsc());
+        $grupos = grupos_tutoria::get_grupos_tutoria($this->get_categoria_turma_ufsc(), $this->tutores_selecionados);
+        $relationship = grupos_tutoria::get_relationship_tutoria($this->get_categoria_turma_ufsc());
+        $cohort_estudantes = grupos_tutoria::get_relationship_cohort_estudantes($relationship->id);
 
-        $associativo_atividades = loop_atividades_e_foruns_de_um_modulo($query_atividades_nao_postadas, $query_forum, $query_quiz);
+        $course_id = $this->get_curso_moodle();
 
         $dados = array();
 
-        foreach ($associativo_atividades as $grupo_id => $array_dados) {
-
+        // Para cada grupo de tutoria
+        foreach ($grupos as $grupo) {
             $estudantes = array();
-            foreach ($array_dados as $id_aluno => $aluno) {
-                $dados_modulos = array();
 
-                $lista_atividades[] = new report_unasus_student($nomes_estudantes[$id_aluno], $id_aluno, $this->get_curso_moodle(), $aluno[0]->polo, $aluno[0]->cohort);
+            foreach ($this->atividades_cursos as $courseid => $atividades) {
+                foreach ($atividades as $atividade) {
 
-                foreach ($aluno as $atividade) {
-                    /** @var report_unasus_data $atividade */
-
-                    $full_grade[$id_aluno] = grade_get_course_grade($id_aluno, $atividade->source_activity->course_id);
-
-                    if(isset($full_grade[$id_aluno])){
-                        $final_grade = $full_grade[$id_aluno]->str_grade;
-                    } else if(empty($final_grade)){
-                        $final_grade = 'Não há atividades avaliativas para o módulo';
+                    switch (get_class($atividade)) {
+                        case 'report_unasus_assign_activity':
+                            $params = array(
+                                'courseid' => $courseid,
+                                'enrol_courseid' => $courseid,
+                                'assignmentid' => $atividade->id,
+                                'assignmentid2' => $atividade->id,
+                                'relationship_id' => $relationship->id,
+                                'cohort_relationship_id' => $cohort_estudantes->id,
+                                'grupo' => $grupo->id);
+                            $query = query_atividades();
+                            break;
+                        case 'report_unasus_forum_activity':
+                            $params = array(
+                                'courseid' => $courseid,
+                                'enrol_courseid' => $courseid,
+                                'relationship_id' => $relationship->id,
+                                'cohort_relationship_id' => $cohort_estudantes->id,
+                                'grupo' => $grupo->id,
+                                'forumid' => $atividade->id);
+                            $query = query_postagens_forum();
+                            break;
+                        case 'report_unasus_quiz_activity':
+                            $params = array(
+                                'assignmentid' => $atividade->id,
+                                'assignmentid2' => $atividade->id,
+                                'courseid' => $courseid,
+                                'enrol_courseid' => $courseid,
+                                'relationship_id' => $relationship->id,
+                                'cohort_relationship_id' => $cohort_estudantes->id,
+                                'grupo' => $grupo->id,
+                                'forumid' => $atividade->id);
+                            $query = query_quiz();
+                            break;
+                        default:
+                            break;
                     }
 
-                    // para cada novo modulo ele cria uma entrada de dado com o número de atividades daquele modulo
-                    if (!array_key_exists($atividade->source_activity->course_id, $dados_modulos)) {
-                        $dados_modulos[$atividade->source_activity->course_id] = new dado_modulos_concluidos(sizeof($modulos), $final_grade, $atividade);
+                    $result = $DB->get_records_sql($query, $params);
+
+                    foreach ($result as $r){
+                        // Evita que o objeto do estudante seja criado em toda iteração do loop
+                        if (!(isset($lista_atividades[$r->userid][0]))) {
+                            $lista_atividades[$r->userid][] = new report_unasus_student($nomes_estudantes[$r->userid], $r->userid, $course_id, $r->polo, $r->cohort);
+                        }
+
+                        $full_grade[$r->userid] = grade_get_course_grade($r->userid, $course_id);
+
+                        if(isset($full_grade[$r->userid])){
+                            $final_grade = $full_grade[$r->userid]->str_grade;
+                        } else if(empty($final_grade)){
+                            $final_grade = 'Não há atividades avaliativas para o módulo';
+                        }
+                        if (!array_key_exists($course_id, $lista_atividades)) {
+                            $lista_atividades[$r->userid][$course_id] = new dado_modulos_concluidos(sizeof($modulos), $final_grade, $atividade);
+                        }
                     }
 
-                    // para cada atividade nao feita ele adiciona uma nova atividade nao realizada naquele modulo
-                    if ($atividade->source_activity->has_submission()
-                            && !$atividade->has_submitted()
-                            && !$atividade->is_a_future_due()
-                            && !$atividade->has_grade()
-                            && $atividade->is_activity_pending()
-                            && $atividade->is_member_of($this->agrupamentos_membros))
-                    {
-                        $dados_modulos[$atividade->source_activity->course_id]->add_atividade_nao_realizada();
-                        $dados_modulos[$atividade->source_activity->course_id]->add_atividades_pendentes($atividade->source_activity);
-                    }
+                    // Auxiliar para agrupar tutores corretamente
+                    $estudantes = $lista_atividades;
                 }
-
-                $atividades_nao_realizadas_do_estudante = 0;
-                foreach ($dados_modulos as $key => $modulo) {
-                    $lista_atividades[] = $modulo;
-                    $atividades_nao_realizadas_do_estudante += $modulo->get_total_atividades_nao_realizadas();
-                }
-
-                $estudantes[] = $lista_atividades;
-
-                // Unir os alunos de acordo com o polo deles
-                if ($this->agrupar_relatorios == AGRUPAR_POLOS) {
-                    $dados[$nomes_polos[$lista_atividades[0]->polo]][] = $lista_atividades;
-                }
-                // Unir os alunos de acordo com o cohort deles
-                if ($this->agrupar_relatorios == AGRUPAR_COHORTS) {
-                    $key = isset($lista_atividades[0]->cohort) ? $nomes_cohorts[$lista_atividades[0]->cohort] : get_string('cohort_empty', 'report_unasus');
-                    $dados[$key][] = $lista_atividades;
-                }
-                $lista_atividades = null;
             }
 
-            // Ou unir os alunos de acordo com o tutor dele
             if ($this->agrupar_relatorios == AGRUPAR_TUTORES) {
-                $dados[grupos_tutoria::grupo_tutoria_to_string($this->get_categoria_turma_ufsc(), $grupo_id)] = $estudantes;
+                $dados[grupos_tutoria::grupo_tutoria_to_string($this->get_categoria_turma_ufsc(), $grupo->id)] = $estudantes;
             }
+
+            $lista_atividades = null;
         }
 
         return $dados;
