b69c50b922e654fab1f8ce4fe33afa57cfce3044
Roberto Cunha <robertosilvino@gmail.com>
2016-02-23 16:25:52 -0300

    ajuste de versão

--- version.php
+++ version.php
@@ -1,13 +1,13 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2016021205; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2016022301; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2014111006; // Requires this Moodle version (2.8.6)
 $plugin->component = 'report_unasus'; // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array(
     'local_tutores' => 2014091800,
     'local_ufsc' => 2014091800,
-    'local_report_config' => 2015122103
+    'local_report_config' => 2016022301
 );
 
 $maturity = MATURITY_STABLE;             // This version's maturity level.
