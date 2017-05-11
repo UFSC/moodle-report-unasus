a78ef26f90e4d6833434707847d133ba4155c4bd
robertosilvino <robertosilvino@gmail.com>
2017-05-11 16:50:24 -0300

    ajuste nas permissões e na verificação de acesso aos relatórios

--- db/access.php
+++ db/access.php
@@ -4,13 +4,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
-    'report/unasus:view' => array(
-        'riskbitmask' => RISK_PERSONAL,
-        'captype' => 'read',
-        'contextlevel' => CONTEXT_COURSECAT,
-        'archetypes' => array()
-    ),
-
     'report/unasus:view_tutoria' => array(
         'riskbitmask' => RISK_PERSONAL,
         'captype' => 'read',
--- lib.php
+++ lib.php
@@ -6,28 +6,10 @@ defined('MOODLE_INTERNAL') || die;
  * Lista contendo todos relatórios
  */
 function report_unasus_relatorios_validos_list() {
-    return array(
-        'estudante_sem_atividade_avaliada',
-        'estudante_sem_atividade_postada',
-        'modulos_concluidos',
-        'avaliacoes_em_atraso',
-        'atividades_nota_atribuida',
-        'atividades_concluidas_agrupadas',
-        'entrega_de_atividades',
-        'atividades_vs_notas',
-        // - relatório desativado segundo o ticket #4460 'historico_atribuicao_notas',
-        'boletim',
-        'acesso_tutor',
-        'uso_sistema_tutor',
-        'potenciais_evasoes',
-        // Relatórios desativados para nova versão do TCC - ticket #7528
-        /*'tcc_portfolio_consolidado',
-        'tcc_portfolio_concluido',
-        'tcc_portfolio_entrega_atividades',*/
-
-        'tcc_consolidado',
-        'tcc_entrega_atividades',
-        'tcc_concluido');
+    // Função usda no report_unasus_factory para validações
+    return array_merge(report_unasus_relatorios_validos_tutoria_list(),
+        report_unasus_relatorios_restritos_list(),
+        report_unasus_relatorios_validos_orientacao_list());
 }
 
 /*
@@ -43,15 +25,9 @@ function report_unasus_relatorios_validos_tutoria_list() {
         'atividades_concluidas_agrupadas',
         'entrega_de_atividades',
         'atividades_vs_notas',
-        // - relatório desativado segundo o ticket #4460 'historico_atribuicao_notas',
         'boletim',
-        'acesso_tutor',
-        'uso_sistema_tutor',
-        'potenciais_evasoes',
-        // Relatórios desativados para nova versão do TCC - ticket #7528
-        /*'tcc_portfolio_consolidado',
-        'tcc_portfolio_concluido',
-        'tcc_portfolio_entrega_atividades',*/);
+//        'potenciais_evasoes',
+    );
 }
 
 /*
@@ -75,34 +51,36 @@ function report_unasus_relatorios_restritos_list() {
  */
 function report_unasus_extend_navigation_course($navigation, $course, $context) {
 
-    $restricted_list = report_unasus_relatorios_restritos_list();
+    $reports = array();
 
-    if ($course->id != SITEID && has_capability('report/unasus:view', $context)) {
+    //Caso usuário seja tutor
+    if( ($course->id != SITEID && has_capability('report/unasus:view_all', $context)) ||
+        ($course->id != SITEID && has_capability('report/unasus:view_tutoria', $context))
+      ) {
+        $reports = array_merge($reports, report_unasus_relatorios_validos_tutoria_list());
+    }
 
-        //Caso usuário seja tutor
-        if(($course->id != SITEID && has_capability('report/unasus:view_tutoria', $context)) &&
-            ($course->id != SITEID && has_capability('report/unasus:view_all', $context))) {
-                $reports = report_unasus_relatorios_validos_tutoria_list();
-        }
+    //Caso usuário seja coordenador
+    if( ($course->id != SITEID && has_capability('report/unasus:view_all', $context))
+      ) {
+        $reports = array_merge($reports, report_unasus_relatorios_restritos_list());
+    }
 
-        //Caso usuário seja orientador
-        if($course->id != SITEID && has_capability('report/unasus:view_orientacao', $context) &&
-            ($course->id != SITEID && has_capability('report/unasus:view_all', $context))) {
-                $reports = report_unasus_relatorios_validos_orientacao_list();
-        }
+    //Caso usuário seja orientador
+    if( ($course->id != SITEID && has_capability('report/unasus:view_all', $context)) ||
+        ($course->id != SITEID && has_capability('report/unasus:view_orientacao', $context))
+      ) {
+        $reports = array_merge($reports, report_unasus_relatorios_validos_orientacao_list());
+    }
 
-        //Caso usuário seja ambos
-        if(($course->id != SITEID && has_capability('report/unasus:view_tutoria', $context)) &&
-            ($course->id != SITEID && has_capability('report/unasus:view_orientacao', $context))) {
-                $reports = report_unasus_relatorios_validos_list();
-        }
+    if( ($course->id != SITEID && has_capability('report/unasus:view_all', $context)) ||
+        ($course->id != SITEID && has_capability('report/unasus:view_tutoria', $context)) ||
+        ($course->id != SITEID && has_capability('report/unasus:view_orientacao', $context))
+    ) {
 
         $unasus_node = $navigation->add(get_string('unasus_navigation_name', 'report_unasus'), null, navigation_node::TYPE_CONTAINER);
-        foreach ($reports as $report) {
-            if (in_array($report, $restricted_list) && !has_capability('report/unasus:view_all', $context)) {
-                continue;
-            }
 
+        foreach ($reports as $report) {
             $url = new moodle_url('/report/unasus/index.php', array('relatorio' => $report, 'course' => $course->id));
             $unasus_node->add(get_string($report, 'report_unasus'), $url, navigation_node::TYPE_SETTING, null, $report, new pix_icon('i/report', ''));
         }
