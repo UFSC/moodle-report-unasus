8b2d1a8490c737c331041e7864fb3dfd14b4db9c
robertosilvino <robertosilvino@gmail.com>
2017-05-12 10:09:25 -0300

    atualizada a versão para 2017051201

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2016060601; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2017051201; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2014111006; // Requires this Moodle version (2.8.6)
 $plugin->component = 'report_unasus'; // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array(
-    'local_tutores' => 2016060601,
+    'local_tutores' => 2017051201,
     'local_relationship' => 2015020100
 );
 
