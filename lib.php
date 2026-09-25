bf50596d11f09523f703fd647826b3acec913b69
Roberto Silvino <robertosilvino@gmail.com>
2026-09-25 09:06:49 -0300

    style: follow moodle-cs in the role scope code and tests

--- lib.php
+++ lib.php
@@ -50,19 +50,24 @@ function report_unasus_relatorios_validos_orientacao_list() {
  * @param int[]|null $tutoresselecionados Tutoring groups after the role scope.
  * @param int[]|null $orientadoresselecionados Orientation groups after the role scope.
  * @return bool
+ * @package report_unasus
  */
 function report_unasus_escopo_vazio($relatorio, $context, $tutoresselecionados, $orientadoresselecionados) {
     if (has_capability('report/unasus:view_all', $context)) {
         return false;
     }
-    if (in_array($relatorio, report_unasus_relatorios_validos_tutoria_list())
-            && has_capability('report/unasus:view_tutoria', $context)
-            && is_array($tutoresselecionados) && empty($tutoresselecionados)) {
+    if (
+        in_array($relatorio, report_unasus_relatorios_validos_tutoria_list())
+        && has_capability('report/unasus:view_tutoria', $context)
+        && is_array($tutoresselecionados) && empty($tutoresselecionados)
+    ) {
         return true;
     }
-    if (in_array($relatorio, report_unasus_relatorios_validos_orientacao_list())
-            && has_capability('report/unasus:view_orientacao', $context)
-            && is_array($orientadoresselecionados) && empty($orientadoresselecionados)) {
+    if (
+        in_array($relatorio, report_unasus_relatorios_validos_orientacao_list())
+        && has_capability('report/unasus:view_orientacao', $context)
+        && is_array($orientadoresselecionados) && empty($orientadoresselecionados)
+    ) {
         return true;
     }
     return false;
